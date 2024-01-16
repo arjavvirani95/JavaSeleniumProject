47e7aac7e427506e020171fd382714a0e7abfbe4
arjavvirani95 <arjav.virani17@gmail.com>
2024-01-16 18:19:40 -0500

    commiting Java programming logical question ans

---
+++ src/test/java/javaProgrammingExamplesInterview/NewPushFile.java
@@ -0,0 +1,13 @@
+package javaProgrammingExamplesInterview;
+
+public class NewPushFile {
+	public static void main(String[] args) {
+		System.out.println("this is new push file");
+		System.out.println("i am changing push file");
+		System.out.println("adding one more print line from introduction");
+		
+		System.out.println("push from ARJAV EST zone");
+		System.out.println("arjav");
+		System.out.println("Automation Engineer");
+	}
+}
